b3663613a3cb93f4e83a12db20888beaad6e995f
tahoangphuoc <tahoangphuocbp@gmail.com>
2024-04-01 22:16:59 +0700

    lam menu partial

---
+++ ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XayDungWebAphone.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using XayDungWebAphone.Models;
+
+namespace XayDungWebAphone.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<User> Users { get; set; }
+    /*    public List<Blog> Blogs { get; set; }
+        public List<Slider> Sliders { get; set; }*/
+        public List<Product> CatProds { get; set; }
+        public List<Product> DogProds { get; set; }
+    }
+
+}
